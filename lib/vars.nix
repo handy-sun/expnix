f3a5d155d2ef4970d1e6f40315befa41dfc05215
sooncheer <handy-sun@foxmail.com>
2026-03-13 23:02:44 +0800

    little patch for 'collect config.nix into nix-core.nix; refine something for both nixos and darwin'

--- lib/vars.nix
+++ lib/vars.nix
@@ -12,5 +12,9 @@ rec {
     VISUAL = "nvim";
     RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup";
     RUSTUP_UPDATE_ROOT = "https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup";
+    ## some XDG vars aren't set, the missing ones are now set according to the
+    ## spec: (https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html)
+    XDG_STATE_HOME = "$HOME/.local/state";
+    XDG_CACHE_HOME = "$HOME/.cache";
   };
 }
