4fa10081f55ed5cb86f4bf0be40f854e91312cd0
sooncheer <handy-sun@foxmail.com>
2026-04-02 21:38:08 +0800

    hm: enable programs.helix

--- home/tui/editor.nix
+++ home/tui/editor.nix
@@ -1,4 +1,6 @@
 {
+  lib,
+  pkgs,
   inputs,
   isDarwin,
   isHmSingle,
@@ -8,7 +10,7 @@ let
   isNeedBuildEnv = (!isDarwin && !isHmSingle);
 in
 {
-  ## donnot use programs.vim
+  ## Use `xdg.configFile."vim/"` instead of programs.vim
   xdg.configFile = {
     "vim/vimrc".source     = inputs.my-dotvim + "/vimrc";
     "vim/user2.vim".source = inputs.my-dotvim + "/user2.vim";
@@ -19,5 +21,30 @@ in
     setBuildEnv = isNeedBuildEnv;  # Only needed for NixOS
     withBuildTools = isNeedBuildEnv; # Only needed for NixOS
   };
-}
 
+  programs.helix = {
+    enable = true;
+
+    settings = {
+      theme = "autumn_night";
+      editor = {
+        line-number = "relative";
+        cursorline = true;
+        color-modes = true;
+        lsp.display-messages = true;
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        indent-guides.render = true;
+     };
+    };
+
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+    }];
+  };
+}
