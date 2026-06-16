034fa3c845d1ab9278dc4888ba65c8117a7e2748
sooncheer <handy-sun@foxmail.com>
2026-06-16 16:12:35 +0800

    refactor: replace fish loginShellInit with home.sessionPath and comment out hosts config
    - shells.nix: enable home.sessionPath (covers bash/zsh/fish), remove fish loginShellInit
    - mksysmgr.nix: comment out environment.etc."hosts" config block

--- home/tui/shells.nix
+++ home/tui/shells.nix
@@ -9,12 +9,12 @@ let
   enableGenericLinuxPath = isHmSingle;
 in
 {
-  # home.sessionPath = lib.mkIf enableGenericLinuxPath [
-  #   "\${NIX_STATE_DIR:-/nix/var/nix}/profiles/default/bin"
-  #   "${config.home.profileDirectory}/bin"
-  #   "/run/current-system/sw/bin"
-  #   "/run/system-manager/sw/bin"
-  # ];
+  home.sessionPath = lib.mkIf enableGenericLinuxPath [
+    "\${NIX_STATE_DIR:-/nix/var/nix}/profiles/default/bin"
+    "${config.home.profileDirectory}/bin"
+    "/run/current-system/sw/bin"
+    "/run/system-manager/sw/bin"
+  ];
 
   programs.bash = {
     enable = true;
@@ -27,16 +27,6 @@ in
 
   programs.fish = {
     enable = true;
-    loginShellInit = lib.mkIf enableGenericLinuxPath ''
-      set -l nix_state_dir /nix/var/nix
-      set -q NIX_STATE_DIR; and set nix_state_dir $NIX_STATE_DIR
-
-      fish_add_path --path --prepend \
-        $nix_state_dir/profiles/default/bin \
-        ${config.home.profileDirectory}/bin \
-        /run/current-system/sw/bin \
-        /run/system-manager/sw/bin
-    '';
     plugins = [
       {
         name = "tide";
--- lib/mksysmgr.nix
+++ lib/mksysmgr.nix
@@ -99,24 +99,24 @@ inputs.system-manager.lib.makeSystemConfig {
           ignoreShellProgramCheck = true;
         };
 
-        environment.etc."hosts" = {
-          text = ''
-            127.0.0.1 localhost
-            ::1 localhost ip6-localhost ip6-loopback
-            127.0.1.1 ${hostName}
+        # environment.etc."hosts" = {
+        #   text = ''
+        #     127.0.0.1 localhost
+        #     ::1 localhost ip6-localhost ip6-loopback
+        #     127.0.1.1 ${hostName}
 
-            # The following lines are desirable for IPv6 capable hosts
-            ::1     ip6-localhost ip6-loopback
-            fe00::0 ip6-localnet
-            ff00::0 ip6-mcastprefix
-            ff02::1 ip6-allnodes
-            ff02::2 ip6-allrouters
+        #     # The following lines are desirable for IPv6 capable hosts
+        #     ::1     ip6-localhost ip6-loopback
+        #     fe00::0 ip6-localnet
+        #     ff00::0 ip6-mcastprefix
+        #     ff02::1 ip6-allnodes
+        #     ff02::2 ip6-allrouters
 
-            ## expnix managed hosts
-            ${networkingVars.hostsText}
-          '';
-          replaceExisting = true;
-        };
+        #     ## expnix managed hosts
+        #     ${networkingVars.hostsText}
+        #   '';
+        #   replaceExisting = true;
+        # };
 
         environment.etc."ssh/ssh_known_hosts".text = networkingVars.ssh.knownHostsText;
 
