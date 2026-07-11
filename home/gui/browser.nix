f36ad4d6e41c8422528dfb2ceaf1a0fc08069694
sooncheer <handy-sun@foxmail.com>
2026-07-11 23:33:58 +0800

    feat(browser): add Nix-managed Helium
    Package the official Helium AppImage with its desktop metadata and configure it as the default handler for web links on x86_64 Linux GUI hosts.

--- home/gui/browser.nix
+++ home/gui/browser.nix
@@ -1,15 +1,40 @@
 {
   lib,
   config,
+  pkgs,
+  myutils,
   profileLevel,
   ...
 }:
+let
+  enableHelium = profileLevel.guiHeavy && pkgs.stdenv.hostPlatform.system == "x86_64-linux";
+  helium = pkgs.callPackage (myutils.relativeToRoot "packages/helium.nix") { };
+in
 lib.mkIf profileLevel.guiBase {
+  home.packages = lib.optionals enableHelium [ helium ];
+
   programs.chromium = {
-    enable = true;
+    enable = profileLevel.guiHeavy;
   };
   programs.firefox = {
     enable = profileLevel.guiHeavy;
     configPath = config.xdg.configHome + "/mozilla/firefox";
+  };
+
+  xdg = lib.mkIf enableHelium {
+    dataFile = {
+      "applications/helium.desktop".source = "${helium}/share/applications/helium.desktop";
+      "icons/hicolor/256x256/apps/helium.png".source =
+        "${helium}/share/icons/hicolor/256x256/apps/helium.png";
+    };
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = [ "helium.desktop" ];
+        "x-scheme-handler/http" = [ "helium.desktop" ];
+        "x-scheme-handler/https" = [ "helium.desktop" ];
+      };
+    };
   };
 }
---
+++ packages/helium.nix
@@ -0,0 +1,35 @@
+{
+  appimageTools,
+  fetchurl,
+  lib,
+}:
+
+let
+  pname = "helium";
+  version = "0.14.4.1";
+  src = fetchurl {
+    url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
+    hash = "sha256-91hO0NtjUxEZwUyMYe7RD1RfFIelYa8ExzLKRsLaZZo=";
+  };
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/helium.desktop \
+      $out/share/applications/helium.desktop
+    install -m 444 -D ${appimageContents}/helium.png \
+      $out/share/icons/hicolor/256x256/apps/helium.png
+  '';
+
+  meta = {
+    description = "Private, fast, and user-friendly Chromium browser";
+    homepage = "https://helium.computer/";
+    license = lib.licenses.gpl3Only;
+    mainProgram = "helium";
+    platforms = [ "x86_64-linux" ];
+  };
+}
