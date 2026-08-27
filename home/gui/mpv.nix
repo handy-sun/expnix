949b4d24977a9a20d25dd7eda64b8dc9a8b8ed51
sooncheer <handy-sun@foxmail.com>
2026-08-27 20:16:55 +0800

    feat(mpv): add polished UI and playback scripts

--- home/gui/mpv.nix
+++ home/gui/mpv.nix
@@ -5,11 +5,15 @@
   isDarwin,
   ...
 }:
-
 lib.mkIf profileLevel.guiBase {
   programs.mpv = {
     enable = true;
-    scripts = lib.optionals (!isDarwin) [ pkgs.mpvScripts.mpris ];
+    scripts = [
+      pkgs.mpvScripts.uosc
+      pkgs.mpvScripts.thumbfast
+      pkgs.mpvScripts.autoload
+    ]
+    ++ lib.optionals (!isDarwin) [ pkgs.mpvScripts.mpris ];
     config = {
       hwdec = "auto-safe";
       hwdec-codecs = "all";
@@ -17,8 +21,18 @@ lib.mkIf profileLevel.guiBase {
       audio-file-auto = "fuzzy";
       profile = "gpu-hq";
       log-file = "/tmp/mpv.log";
-      volume = 30;
+      volume = 66;
       gpu-shader-cache-dir = "~/.cache/mpv_shaders_cache";
+      osc = false;
+      osd-font = "Noto Sans CJK SC";
+      osd-font-provider = "fontconfig";
+      osd-font-size = 24;
+      osd-color = "#FFE6E6E6";
+      osd-back-color = "#CC1F1F28";
+      osd-border-style = "background-box";
+      osd-outline-size = 0;
+      osd-margin-x = 32;
+      osd-margin-y = 24;
     };
   };
 }
