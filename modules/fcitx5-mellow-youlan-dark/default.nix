7f269236134481b1dc2d01a49840c53eebc74d20
sooncheer <handy-sun@foxmail.com>
2026-09-22 09:56:50 +0800

    fix(fcitx5): pre-rasterize mellow panel SVG filter shadow as bitmap
    Qt SVG re-rasterizes the feGaussianBlur shadow on every candidate
    repaint, spiking CPU on each keystroke (upstream issue #6). Bake the
    4x raster into the SVG as a data-URI image; theme.conf margins stay
    valid. Applied live too; drop this once upstream ships bitmap assets.

--- modules/fcitx5-mellow-youlan-dark/default.nix
+++ modules/fcitx5-mellow-youlan-dark/default.nix
@@ -11,6 +11,8 @@ let
     pname = "fcitx5-mellow-youlan-dark";
     version = "0-unstable-2026-05-30";
 
+    nativeBuildInputs = [ pkgs.librsvg ];
+
     src = pkgs.fetchFromGitHub {
       owner = "sanweiya";
       repo = "fcitx5-mellow-themes";
@@ -19,12 +21,25 @@ let
     };
 
     installPhase = ''
-      runHook preInstall
+            runHook preInstall
 
-      mkdir -p $out/share/fcitx5/themes
-      cp -r mellow-youlan-dark $out/share/fcitx5/themes/
+            mkdir -p $out/share/fcitx5/themes
+            cp -r mellow-youlan-dark $out/share/fcitx5/themes/
+            T=$out/share/fcitx5/themes/mellow-youlan-dark
+            # Embed the SVG filter shadow as a 4x bitmap: Qt SVG re-rasterizes
+            # feGaussianBlur on every keystroke repaint (upstream issue #6).
+            rsvg-convert -w 124 -h 124 $T/panel.svg -o panel-4x.png
+            B64=$(base64 -w0 panel-4x.png)
+            printf '%s%s%s' \
+              '<?xml version="1.0" encoding="UTF-8"?>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="31" height="31" viewBox="0 0 31 31">
+        <image x="0" y="0" width="31" height="31" xlink:href="data:image/png;base64,' \
+              "$B64" \
+              '"/>
+      </svg>
+      ' > $T/panel.svg
 
-      runHook postInstall
+            runHook postInstall
     '';
   };
 in
